ac5e6aa1114d9a9968f2699eb0ea3bdca1a132ae
Matan Vardi <matanvardi@Matans-Mac.local>
2015-03-08 14:35:16 -0700

    Initial Commit

---
+++ MovieMarathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieMarathon
+//
+//  Created by Matan Vardi on 3/8/15.
+//  Copyright (c) 2015 Matan Vardi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
